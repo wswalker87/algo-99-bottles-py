d988f77cd6d218f19e391d531d92d6237dd1e086
wswalker87 <scott@wscottwalker.com>
2025-11-12 21:32:51 -0600

    init commit

--- bottles.py
+++ bottles.py
@@ -1,7 +1,16 @@
 def bottle_song():
-	# write your code here!
-	pass
+	finished_drinking = "No more bottles of beer on the wall, no more bottles of beer."
+	empty_drinking = "Go to the store and buy some more, 99 bottles of beer on the wall"
+	
+	before_drinking_string = (f"{current_num_of_beers} bottle of beer on the wall, {current_num_of_beers} bottles of beer")
+	# print(before_drinking_string)
+	after_drinking_string = (f"Take one down and pass it around, {current_num_of_beers} on the wall")
+	# print(after_drinking_string)
+	
+	while current_num_of_beers > 0:
+		current_num_of_beers = 99
+		print(before_drinking_string)
+		current_num_of_beers -= 1
+		print(after_drinking_string)
 
 bottle_song()
-
-####kldjsfalkfdajalskdfjdfls
